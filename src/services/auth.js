3e5d2cb747aee45f6c4a499fa56cfcfee7d73fff
N Karthik Reddy <karthikreddytoyou@gmail.com>
2026-03-07 18:30:08 +0530

    connecting to backend

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://job-application-tracker-backend-c62w.onrender.com",
   withCredentials: true 
 });
 
